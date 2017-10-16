f6228d2c27a93e2b2c12e9666bcd50eddb63369b
nievescanas <nievescanas@github.com>
2017-10-16 22:49:58 +0200

    Pasado por pep8

--- karaoke.py
+++ karaoke.py
@@ -5,43 +5,58 @@ import urllib.request
 import json
 import sys
 from xml.sax import make_parser
-from xml.sax.handler import ContentHandler
-import smallsmilhandler  
+import smallsmilhandler
 
-class  ImpresionKaraoke(smallsmilhandler.SmallSMILHandler):
 
-    def Url(self): 
-        for dicc in self.etiquetas:
-            if 'src' in dicc and 'http://' in dicc['src']:
-                 with urllib.request.urlopen(dicc['src']) as response:
-                    name = dicc['src']
-                    name = name[name.rfind('/')+1:]
-                    local_filename, headers = urllib.request.urlretrieve(dicc['src'],filename=name)
-                    html = open(local_filename)
-                    print (html)
+class KaraokeLocal():
 
-    def WriteLine(self):
+    def __init__(self):
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(sys.argv[1]))
+        self.etiquetas = cHandler.get_tags()
+
+    def __str__(self):
+        objeto = ''
         for etiqueta in self.etiquetas:
-            print ("{0:12}".format(etiqueta['name']), end="")
+            linea = str(etiqueta['name'])
 
             for atributo in etiqueta:
                 if etiqueta[atributo] != etiqueta['name'] and etiqueta[atributo] != "":
-                    print(atributo,"=", "{0:12}".format(etiqueta[atributo]), end="")
-            print("")
- 
-    def WritelineJson(self):
-        with open('karaoke.json', 'w') as outfile:
-            json.dump(self.etiquetas, outfile)
+                    linea += ('{tabulador}{atr}="{valor}"'.format(atr=atributo, valor=etiqueta[atributo], tabulador='\t'))
+            objeto += ('{line}{salto}'.format(line=linea, salto='\n'))
+        return(objeto)
+
+    def do_json(self, name=''):
+        if name == "":
+            name = fich[:fich.find('.')]
+            name = name + ".json"
+
+        with open(name, 'w') as outfile:
+            json.dump(self.etiquetas, outfile, separators=(',', ':'), indent="")
+
+    def do_local(self):
+        for dicc in self.etiquetas:
+            if 'src' in dicc and 'http://' in dicc['src']:
+                with urllib.request.urlopen(dicc['src']) as response:
+                    name = dicc['src']
+                    name = name[name.rfind('/')+1:]
+                    urllib.request.urlretrieve(dicc['src'], filename=name)
+                    dicc['src'] = name
 
 
 if __name__ == "__main__":
-    
-    parser = make_parser()
-    cHandler = ImpresionKaraoke()
-    parser.setContentHandler(cHandler)
-    parser.parse(open(sys.argv[1]))
-    cHandler.Url()
-    cHandler.WriteLine()
-    cHandler.WritelineJson()
 
+    try:
+        fich = sys.argv[1]
+    except IndexError:
+        sys.exit("Usage:python3 karaoke.py file.smil.")
 
+    karaoke = KaraokeLocal()
+    print(karaoke)
+    karaoke.do_json()
+    karaoke.do_local()
+
+    karaoke.do_json('local.json')
+    print(karaoke)
--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,29 +4,29 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
 
-        self.etiquetas = []    
-        self.dicc={
+        self.etiquetas = []
+        self.dicc = {
             'root-layout': ['width', 'height', 'background-color'],
             'region': ['id', 'top', 'bottom', 'left', 'right'],
             'img': ['src', 'region', 'begin', 'dur'],
-            'audio' : ['src', 'begin', 'dur'],
-            'textstream' : ['src', 'begin', 'dur']          
-    }
+            'audio': ['src', 'begin', 'dur'],
+            'textstream': ['src', 'begin', 'dur']}
 
     def startElement(self, name, attrs):
         dicc = {}
         if name in self.dicc:
-            dicc['name']=name
+            dicc['name'] = name
             for atributo in self.dicc[name]:
                     dicc[atributo] = attrs.get(atributo, "")
             self.etiquetas.append(dicc)
 
     def get_tags(self):
-        return self.etiquetas         
+        return self.etiquetas
 
 if __name__ == "__main__":
 
@@ -34,6 +34,3 @@ if __name__ == "__main__":
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open())
-
-
-
