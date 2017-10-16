45de5463a5a641b406ddb58287c48e1aabb29060
Nieves Canas Martin <nievescm@alpha26.aulas.gsyc.urjc.es>
2017-10-16 13:45:33 +0200

    Incluido descarga de las url

--- karaoke.py
+++ karaoke.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import urllib.request
 import json
 import sys
 from xml.sax import make_parser
@@ -8,6 +9,16 @@ from xml.sax.handler import ContentHandler
 import smallsmilhandler  
 
 class  ImpresionKaraoke(smallsmilhandler.SmallSMILHandler):
+
+    def Url(self): 
+        for dicc in self.etiquetas:
+            if 'src' in dicc and 'http://' in dicc['src']:
+                 with urllib.request.urlopen(dicc['src']) as response:
+                    name = dicc['src']
+                    name = name[name.rfind('/')+1:]
+                    local_filename, headers = urllib.request.urlretrieve(dicc['src'],filename=name)
+                    html = open(local_filename)
+                    print (html)
 
     def WriteLine(self):
         for etiqueta in self.etiquetas:
@@ -17,7 +28,7 @@ class  ImpresionKaraoke(smallsmilhandler.SmallSMILHandler):
                 if etiqueta[atributo] != etiqueta['name'] and etiqueta[atributo] != "":
                     print(atributo,"=", "{0:12}".format(etiqueta[atributo]), end="")
             print("")
-
+ 
     def WritelineJson(self):
         with open('karaoke.json', 'w') as outfile:
             json.dump(self.etiquetas, outfile)
@@ -29,7 +40,8 @@ if __name__ == "__main__":
     cHandler = ImpresionKaraoke()
     parser.setContentHandler(cHandler)
     parser.parse(open(sys.argv[1]))
-    cHandler.WriteLine()
-    cHandler.WritelineJson()
+    cHandler.Url()
+    #cHandler.WriteLine()
+    #cHandler.WritelineJson()
 
 
